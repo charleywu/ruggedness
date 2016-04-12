248a4b32e939a5a495dba3ebc90d75b639844f92
charleywu <charleymswu@gmail.com>
2016-04-12 17:06:42 +0200

    finished Mason and Watts

--- environments/functions.R
+++ environments/functions.R
@@ -752,40 +752,28 @@ nk <- function(xx){
   return(y) 
 }
 #environment from Mason & Watts
-#todo: Do we keep it as a 100x100 grid, or do we interpolate it to fit on a 1000 x 1000 grid to be consistent with out simulations?
 library(akima)
-MasonWatts<-function(){
-  L <-100 #LxL grid
+MasonWatts<-function(L){
   R <- 3 #variance term
-  rho <- .7
+  rho <- 0.7
   #1. create function matrix as a unimodal bivariate Gaussian with the mean randomly chosen, with variance R
   #todo
   fitnessMatrix <- matrix(0, ncol=L, nrow =L)
   #2. compute psuedorandom Perlin noise
   #2a loop through octaves and randomly draw values
   for (omega in 3:7){
-    octave <- 2^omega
-    octaveMatrix <- matrix(0,ncol=L, nrow=L)
-    for (x in seq(1,L,octave)){
-      for (y in seq(1,L,octave)){
-        #assign location in octave matrix a random number
-        octaveMatrix[x,y] <-  runif(1, min = 0, max = 1)
-      }
-    }
+    octave <- 2^omega  #scale octave to account for grids larger than original 100x100
+    #create a smaller matrix, containing only randomly assigned payoffs corresponding to the cells affected by the octave
+    octaveMatrix <- matrix(runif(octave^2),ncol=octave, nrow=octave)
+    #center octave sequence on median of grid
+    octaveSeq <- seq(1,L, length.out=octave)
     #2b. smooth values of all cell values using bicubic interpolation
-    #vector of values to interpolate, that were not in the octave
-    notInOctave <- seq(1:L) [! seq(1:L) %in% seq(1,L,octave)]
-    #TODO: get this to work
-    #http://www.inside-r.org/packages/cran/akima/docs/bicubic.grid
-    octaveMatrix <- bicubic.grid(seq(1:L), seq(1:L), octaveMatrix, 1, 1) 
-    
+    octaveMatrix <- bicubic.grid(octaveSeq, octaveSeq, octaveMatrix, c(1,L), c(1,L),1,1) 
     #2c. scale matrix by the persistence paramter  
-    octaveMatrix <- octaveMatrix/rho^omega
-
+    octaveMatrix <- octaveMatrix$z * rho^omega
     #3. sum together
     fitnessMatrix <- fitnessMatrix + octaveMatrix
   }
-
   #3 continued... scale fitnessMatrix to between 1 and 100
   fitnessMatrix <- fitnessMatrix * (100/max(fitnessMatrix))
 
