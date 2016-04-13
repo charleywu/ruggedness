b00e9b4827a5993106f3f655c8da6cd63ff7a55e
Charley Wu <charleymswu@gmail.com>
2016-04-13 12:39:47 +0200

    finished Mason Watts

--- environments/functions.R
+++ environments/functions.R
@@ -754,11 +754,15 @@ nk <- function(xx){
 #environment from Mason & Watts
 library(akima)
 MasonWatts<-function(L){
-  R <- 3 #variance term
+  R <- 3 * (L/100) #variance term
   rho <- 0.7
   #1. create function matrix as a unimodal bivariate Gaussian with the mean randomly chosen, with variance R
-  #todo
-  fitnessMatrix <- matrix(0, ncol=L, nrow =L)
+  #generate two random means
+  X <- dnorm(seq(1,L), mean = runif(1,1,L), sd=sqrt(R))
+  Y <- dnorm(seq(1,L), mean = runif(1,1,L), sd=sqrt(R))
+  fitnessMatrix <- X %*% t(Y)
+  #scale to between 0 and 1 (Not sure if this is what they do)
+  fitnessMatrix <- fitnessMatrix * (1/max(fitnessMatrix))
   #2. compute psuedorandom Perlin noise
   #2a loop through octaves and randomly draw values
   for (omega in 3:7){
@@ -778,4 +782,6 @@ MasonWatts<-function(L){
   fitnessMatrix <- fitnessMatrix * (100/max(fitnessMatrix))
 
   return(fitnessMatrix)
+  #3D plotting example
+  #https://cran.r-project.org/web/packages/plot3D/vignettes/volcano.pdf
 }
